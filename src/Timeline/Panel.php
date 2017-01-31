ace048087badc4bfc3a6480a76c19dab54c40a3a
tchiotludo <tchiot.ludo@gmail.com>
2017-01-31 19:22:48 +0100

    Timeline widget

---
+++ src/Timeline/Panel.php
@@ -0,0 +1,141 @@
+<?php
+
+/*
+ * This file is part of the Сáша framework.
+ *
+ * (c) tchiotludo <http://github.com/tchiotludo>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare (strict_types = 1);
+
+namespace Cawa\Widget\Timeline;
+
+use Cawa\Date\DateTime;
+use Cawa\Renderer\Element;
+use Cawa\Renderer\HtmlContainer;
+use Cawa\Renderer\HtmlElement;
+
+class Panel extends \Cawa\Bootstrap\Components\Panel
+{
+    /**
+     * @param DateTime $date
+     * @param null $content
+     * @param string $type
+     */
+    public function __construct(DateTime $date, $content = null, $type = \Cawa\Bootstrap\Components\Panel::TYPE_DEFAULT)
+    {
+        parent::__construct(null, $content, $type);
+
+        $this->date = $date;
+    }
+
+    /**
+     * @var DateTime
+     */
+    private $date;
+
+    /**
+     * @return DateTime
+     */
+    public function getDate() : DateTime
+    {
+        return $this->date;
+    }
+
+    /**
+     * @param DateTime $date
+     *
+     * @return $this|self
+     */
+    public function setDate(DateTime $date) : self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * @var bool
+     */
+    private $displayTime = true;
+
+    /**
+     * @return bool
+     */
+    public function isDisplayTime() : bool
+    {
+        return $this->displayTime;
+    }
+
+    /**
+     * @param bool $displayTime
+     *
+     * @return Panel
+     */
+    public function setDisplayTime(bool $displayTime) : Panel
+    {
+        $this->displayTime = $displayTime;
+
+        return $this;
+    }
+
+    /**
+     * @var string
+     */
+    private $icon;
+
+    /**
+     * @return string|null
+     */
+    public function getIcon()
+    {
+        return $this->icon;
+    }
+
+    /**
+     * @param string $icon
+     *
+     * @return Panel
+     */
+    public function setIcon(string $icon) : Panel
+    {
+        $this->icon = $icon;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function render()
+    {
+        if ($this->icon) {
+            HtmlContainer::addFirst((new HtmlElement('<div>', $this->icon))
+                ->addClass('panel-heading icon')
+            );
+        }
+
+        if (!$this->getTitle()) {
+            $this->addClass('panel-outline');
+        }
+
+        if ($this->displayTime) {
+            $time = (new HtmlElement('<span>', $this->date->display([null, DateTime::DISPLAY_SHORT])))
+                ->addClass('time')
+                ->render();
+
+            if ($this->getTitle()) {
+                $this->setTitle($time . $this->getTitle());
+            } else if ($this->getContent()) {
+                $this->setContent($time . $this->getContent());
+            } else {
+                $this->container->addFirst(new Element($time));
+            }
+        }
+
+        return parent::render();
+    }
+}
---
+++ src/Timeline/Timeline.php
@@ -0,0 +1,139 @@
+<?php
+
+/*
+ * This file is part of the Сáша framework.
+ *
+ * (c) tchiotludo <http://github.com/tchiotludo>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare (strict_types = 1);
+
+namespace Cawa\Widget\Timeline;
+
+use Cawa\Controller\ViewDataTrait;
+use Cawa\Date\DateTime;
+use Cawa\Renderer\ContainerTrait;
+use Cawa\Renderer\HtmlElement;
+use Cawa\Renderer\Phtml;
+use Cawa\Renderer\PhtmlHtmlElement;
+
+/**
+ * @see http://bootsnipp.com/snippets/featured/single-column-responsive-timeline
+ */
+class Timeline extends PhtmlHtmlElement
+{
+    use ContainerTrait;
+    use ViewDataTrait;
+
+    const SIZE_LG = 'cawa-timeline-lg';
+    const SIZE_SM = 'cawa-timeline-sm';
+
+    /**
+     * @param string|null $size
+     */
+    public function __construct(string $size = null)
+    {
+        parent::__construct('<div>');
+        $this->addClass('cawa-timeline');
+
+        if ($size) {
+            $this->addClass($size);
+        }
+    }
+
+    private $sortDesc = true;
+
+    /**
+     * @return bool
+     */
+    public function isSortDesc() : bool
+    {
+        return $this->sortDesc;
+    }
+
+    /**
+     * @param bool $sortDesc
+     *
+     * @return Timeline
+     */
+    public function setSortDesc(bool $sortDesc) : Timeline
+    {
+        $this->sortDesc = $sortDesc;
+
+        return $this;
+    }
+
+    /**
+     * @param bool $enabled
+     *
+     * @return $this|self
+     */
+    public function setTopRound(bool $enabled) : self
+    {
+        if ($enabled) {
+            $this->removeClass('no-top-round');
+        } else {
+            $this->addClass('no-top-round');
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param bool $enabled
+     *
+     * @return $this|self
+     */
+    public function setBottomRound(bool $enabled) : self
+    {
+        if ($enabled) {
+            $this->removeClass('no-bottom-round');
+        } else {
+            $this->addClass('no-bottom-round');
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function render()
+    {
+        usort($this->elements, function(Panel $a, Panel $b)
+        {
+            return $a->getDate()->getTimestamp() == $b->getDate()->getTimestamp() ? 0 :
+                ($a->getDate()->getTimestamp() > $b->getDate()->getTimestamp() ? 1 : -1);
+        });
+
+        if ($this->sortDesc) {
+            $this->elements = array_reverse($this->elements);
+
+        }
+
+        $content = '';
+        $previousDate = null;
+
+        /** @var Panel $element */
+        foreach ($this->elements as $element) {
+
+            if (is_null($previousDate) || !$element->getDate()->isSameDay($previousDate)) {
+                $content .= (new HtmlElement('<div>'))
+                    ->addClass('date')
+                    ->setContent($element->getDate()->display([DateTime::DISPLAY_SHORT]))
+                    ->render();
+            }
+
+            $previousDate = $element->getDate();
+
+            $content .= $element->render();
+        }
+
+        $this->data['content'] = $content;
+
+        return parent::render();
+    }
+}
